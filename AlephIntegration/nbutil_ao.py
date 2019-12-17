1a071febd394a63e1daefbfd36099cd04acc1f80
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2019-12-17 12:15:39 -0800

    Update nbutil_ao.py

--- AlephIntegration/nbutil_ao.py
+++ AlephIntegration/nbutil_ao.py
@@ -3,22 +3,46 @@
 import os
 import aiohttp
 
-#Main File
-import nekoBot_alephintegration as nb
+# Main File
+import nekoBot_alephintegration as main
 
-class NekoBot():
-    Fname="Neko"
-    Mname="Bot"
-    Lname="Chliese"
-    Fullname=f"{Fname} {Mname} {Lname}"
+
+class aleph:
+    BaseUrl = "http://aleph.neko-bot.net"
+    quest1 = main.toNb.stringOut_Quest1
+
     def __init__(self):
-        self.Fullname=Fullname
-        
+        self.GetAddress = None
+        self.quest1 = aleph.quest1
+        self.firstname = NekoBot.Fname # Default name (when asked)
+        self.lastname = NekoBot.Lname # DO NOT USE WHEN TALKING, only for any sort of dialog box.
+        self.fullname = NekoBot.Fullname # please for the love of god don't use this one hehe
+    def NPCoutput(self,context,playername="Neko",extras=None):
+        context=context.lower()
+        if context.startswith("Helllo"):pass
 
-class HTTP():
+
+class NekoBot:
+    Fname = "Neko"
+    Mname = "Bot"
+    Lname = "Chliese"
+    Fullname = f"{Fname} {Mname} {Lname}"
+    age = 11
+
+    def __init__(self):
+        self.Fullname = NekoBot.Fullname
+
+
+class HTTP:
     osNam = os.name
     osSys = os.system
+
     def __init__(self, headers, url, timeFrame=0):
+        """
+        :param url: URL to get from
+        :param headers: Headers for URL
+        :param timeFrame: Time between HTTP requests (depreciated)
+        """
         self.headers = headers
         self.url = url
         self.timeframe = timeFrame
@@ -33,11 +57,18 @@ class HTTP():
         """
         Mostly a useless thing, meant for time between next HTTP request (dep.)
         """
+        pass
 
-    class get():
-        def __init__(self, UUID=0, UNAME=None):
+    class get:
+        def __init__(self, UUID=0, UNAME=None, QUID=0):
+            """
+            :param UUID: User ID
+            :param UNAME: UserName
+            :param QUID: Quest ID
+            """
             self.UUID = UUID
             self.UNAME = UNAME
+            self.QUID = QUID
 
         def UserStats_FROMID(self):
             if self.UUID is 0 or None:
@@ -45,5 +76,14 @@ class HTTP():
             if self.UUID is not 0 or None:
                 OUT = None
                 return OUT
+
         def UserStats_FROMNAME(self):
             pass
+
+        def QuestStats_FROMNAME(self):
+            pass
+
+        def QuestStats_FROMID(self):
+            if self.QUID is not 0 or None:
+                out = {"output": "ReturnValuesFromHttp", "returnId": self.QUID}
+                return out
