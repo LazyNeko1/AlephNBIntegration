ea3ebb7e3deb02a4967d683e628ae64a2e375423
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2020-01-04 04:18:16 -0800

    Update nekoBot_alephintegration.py

--- AlephIntegration/nekoBot_alephintegration.py
+++ AlephIntegration/nekoBot_alephintegration.py
@@ -1,53 +1,75 @@
 # Discord Bot Integration hosted by NekoBot
 
+
 import aiohttp
+
 import asyncio
+
 import nbapi
+
 from discord.ext import commands
 
 import nbutil_ao as nb
 
 
 # Variable Assignment for UTILS module under NekoBotInt()
-class toUtil:
-    stringOut_Quest1 = None
 
-    def __init__(self):
-        pass
+class toUtil:
+    class stringOut:
+        Quest1 = 1
+        RQ = None
+
+
 
 
 class NekoBotInt:
     nekobot_fullname = nb.NekoBot.Fullname
+
     nekobot_age = nb.NekoBot.age
+
     BASEURL = nb.aleph.BaseUrl
 
     class info:
+
         def __init__(self):
             self.nekobot_fullname = "Neko-Bot-Chliese"
+
             self.nekobot_age = "11"
 
     class post:
-        def __init__(self, url, headers=None, typeOf=None):
-            """
-            :param url: URL to get info from
-            :param headers: Headers for URL
-            :param typeOf: Type of GET (Quests, Stats, etc.)
+
+        def __init__(self, url, headers=None):
             """
 
-            self.HTTP = nb.HTTP(url=url, headers=headers, typeOf=typeOf)
+            :param url: URL to get info from
+
+            :param headers: Headers for URL
+
+
+            """
+
+            self.HTTP = nb.HTTP(url=url, headers=headers)
 
         def STATS(self, UUID=0, stat_3string=None, stat_1line=None):
             pass
 
     class get:
+
         def __init__(self, url, headers=None, typeOf=None):
             """
+
             :param url: URL to get info from
+
             :param headers: Headers for URL
+
             :param typeOf: Type of GET (Quests, Stats, etc.) (None due to it not being used in this specific GET request. That's used by the UTIL lib.)
+
             """
+
             self.needs = typeOf
+
             self.headers = headers
+
             self.url = url
 
         def STATS(self, UUID=0):
@@ -61,6 +83,7 @@ class BOT_out(commands.Cog):
 
     async def quests(self, ctx):
         command = ctx.message.content.split(" ", 1)[1]
+
         if command.startswith("view"):
             print("ViewQuest FROMUTILS")
 
